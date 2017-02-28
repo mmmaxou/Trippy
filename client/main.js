6fc203c7268932697257d2690793d871c0a38955
Pluchard Maximilien <mmmaxou@yahoo.fr>
2017-02-28 12:41:23 +0100

    queries

--- client/main.js
+++ client/main.js
@@ -1,7 +1,16 @@
 Template.queries.events({
-        "click #fullfill": function () {
-            Meteor.call("reset", function (err, res) {
-            if (err) console.log("!!", err);
-            });
-        }
-    });
+    "click #fullfill": function () {
+        Meteor.call("reset", function (err, res) {
+        if (err) console.log("!!", err);
+        });
+    }
+});
+
+Template.hello.helpers({
+    cities : function() {
+        return Cities.find();
+    },
+    activities : function() {
+        return Activities.find();
+    }
+});

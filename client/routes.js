9d6c4b1a95a700efa3c4f30b1c43740f28f781ff
Pluchard Maximilien <mmmaxou@yahoo.fr>
2017-03-01 10:35:44 +0100

    last Fix routes

--- client/routes.js
+++ client/routes.js
@@ -3,25 +3,18 @@ Router.configure({
     notFoundTemplate: "notFound"
 });
 
-Router.route("/", function() {
-    this.render('home');
-}, {
-    layoutTemplate: "home"
+Router.route("/", {
+    template: "home",
 });
 
-Router.route('/activities/:id', function (){
-    this.render('activities', {    
-        data: function () {
-            // Return the document for the selected city (the one whose id is given)
-            // The value of this id is given by this.params.id
-            var id = this.params._id;            
-            return activities.findOne({_id: id});
-        }
-    })
+Router.route('/activities/:id', {
+    template: "activities",
+    data: function () {
+        var id = this.params._id;
+        return Activities.findOne({_id: id});
+    }    
 });
 
-Router.route('/formActivity', function(){
-    this.render("formActivity");
-}, {
-    layoutTemplate: "formActivity"
+Router.route('/formActivity', {
+    template: "formActivity",
 })

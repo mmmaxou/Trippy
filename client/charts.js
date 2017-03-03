712254fcf41f72a57ed3b88907a33e0d19e992f5
Draelnir <g.lollier@hotmail.fr>
2017-03-03 12:01:09 +0100

    CHARTS ARE FUCKING WORKING

--- client/charts.js
+++ client/charts.js
@@ -7,28 +7,48 @@ Template.charts.onRendered(function () {
     console.log("currentData = ",Template.currentData().name.replace(/ /g, "-"));
     console.log("parrentData = ",Template.parentData(1).name.replace(/ /g, "-"));
     let ctx = $("#myChart").get(0).getContext("2d");
-    Meteor.call("getWeatherData", ct,
-      function (err, result) {
+    Meteor.call("getWeatherData", ct, (err, result) => {
         let data = {
         labels: [],
         datasets: []
       };
       data.datasets.push({
-        label: "temp min",
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: []
-
+          label: "temp",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: []
       });
-      for (let d of result) {
+      data.datasets.push({
+          label: "temp min",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: []
+      });
+      data.datasets.push({
+          label: "temp max",
+          fillColor: "rgba(255,187,205,0.2)",
+          strokeColor: "rgba(255,187,205,1)",
+          pointColor: "rgba(255,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(255,187,205,1)",
+          data: []
+      });
+      for (let d of result.list) {
         data.labels.push(d.dt_txt);
         data.datasets[0].data.push(d.main.temp);
+        data.datasets[1].data.push(d.main.temp_min);
+        data.datasets[2].data.push(d.main.temp_max);
       }
-      let myChart = new Chart(myPicture).Line(data, Chart.defaults.global);
+      new Chart(ctx).Line(data, Chart.defaults.global);
     });
 
   });

be2fc3b6cc472fe7453a48a0a3a7bd055ccf4046
mmmaxou <mmmaxou@yahoo.fr>
2017-05-11 10:52:16 +0200

    quick commit

--- client/main.js
+++ client/main.js
@@ -8,7 +8,10 @@ Template.queries.events({
     "click #fullfill": function() {
         Meteor.call("reset", function (err, res) {
             if (err)
-                console.log("!!", err);
+                {console.log("!!", err);}
+            else {
+                console.log(res)
+            }
         });
     }
 });
